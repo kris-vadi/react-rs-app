c686cf93879e2667d4eaf9842391baf98daece69
Kristina Morozova <christina.vadi@gmail.com>
2023-10-30 03:55:52 +0600

    feat: new build files

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './styles/App.scss';
-import App from './App';
-
-const root = document.getElementById('root') as HTMLElement;
-
-if (root) ReactDOM.createRoot(root).render(<App />);
