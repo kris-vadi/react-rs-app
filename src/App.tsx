f167be4c198d1a61bbf4225c04fe8b0ca4e73fcb
Kristina Morozova <christina.vadi@gmail.com>
2023-10-30 21:08:04 +0600

    fix: App non func component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import HomePage from './components/HomePage/HomePage';
 import ErrorBoundary from './components/Error/ErrorBoundary';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <ErrorBoundary>
-      <HomePage />
-    </ErrorBoundary>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ErrorBoundary>
+        <HomePage />
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './styles/App.scss';
 import App from './App';
 
 const root = document.getElementById('root') as HTMLElement;
